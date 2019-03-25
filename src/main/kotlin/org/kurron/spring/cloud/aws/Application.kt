055c4496cf9e0520302af3aecc6e84200bfb8641
Ron Kurr <rkurr@overstock.com>
2019-03-25 18:33:12 +0000

    Simplifying the dependencies.

--- src/main/kotlin/org/kurron/spring/cloud/aws/Application.kt
+++ src/main/kotlin/org/kurron/spring/cloud/aws/Application.kt
@@ -1,23 +1,6 @@
 package org.kurron.spring.cloud.aws
 
-import com.samskivert.mustache.Mustache
-import org.springframework.boot.Banner.Mode.OFF
-import org.springframework.boot.CommandLineRunner
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-
-@SpringBootApplication
-@EnableConfigurationProperties(ConfigurationProperties::class)
-class BlogApplication {
-    @Bean
-    fun mustacheCompiler( loader: Mustache.TemplateLoader? ) = Mustache.compiler().escapeHTML( false ).withLoader( loader )!!
-
-}
-
 fun main(args: Array<String>) {
-    runApplication<BlogApplication>(*args) {
-        setBannerMode(OFF)
-    }
+    println(  "Hello!" )
+    args.forEach { println( it ) }
 }
